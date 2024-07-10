e3a07ac365f72d11e6b44bd99139643f135cc775
rahul-sardar <rahulsardar7021@gamil.com>
2024-07-10 21:16:19 +0530

    First Push For Rest Assured Framework

---
+++ src/main/java/com/qa/gorest/utils/JsonPathValidator.java
@@ -0,0 +1,57 @@
+package com.qa.gorest.utils;
+
+import java.util.List;
+import java.util.Map;
+
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
+import com.qa.gorest.frameworkexception.APIFrameworkException;
+
+import io.restassured.response.Response;
+
+public class JsonPathValidator {
+	
+	private String getJsonResponseAsString(Response response) {
+		return response.body().asString();
+	}
+	
+	public <T> T read(Response response, String jsonPath) {
+		String jsonResponse = getJsonResponseAsString(response);
+		try {
+			return JsonPath.read(jsonResponse, jsonPath);
+			
+		}catch(PathNotFoundException e){
+			e.printStackTrace();
+			throw new APIFrameworkException(jsonPath + "Is Not Found");
+			
+		}
+		  
+	}
+	
+	public <T> List<T> readList(Response response, String jsonPath) {
+		String jsonResponse = getJsonResponseAsString(response);
+		try {
+			return JsonPath.read(jsonResponse, jsonPath);
+			
+		}catch(PathNotFoundException e){
+			e.printStackTrace();
+			throw new APIFrameworkException(jsonPath + "Is Not Found");
+			
+		}
+		  
+	}
+	
+	public <T> List<Map<String,T>> readListOfMaps(Response response, String jsonPath) {
+		String jsonResponse = getJsonResponseAsString(response);
+		try {
+			return JsonPath.read(jsonResponse, jsonPath);
+			
+		}catch(PathNotFoundException e){
+			e.printStackTrace();
+			throw new APIFrameworkException(jsonPath + "Is Not Found");
+			
+		}
+		  
+	}
+
+}
---
+++ src/main/java/com/qa/gorest/utils/StringUtils.java
@@ -0,0 +1,7 @@
+package com.qa.gorest.utils;
+
+public class StringUtils {
+	public static String getRandomEmailId() {
+		return "api"+System.currentTimeMillis()+"@api.com";
+	}
+}
